0db24b2c6ba21cdb4fbf3421e425b985533e7f86
Muhammad Samir <79057248+MuhammadSamir1212@users.noreply.github.com>
2022-11-19 18:58:54 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import styles from "./style";
+
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from "./components";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    {/* Navbar */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+    {/* Hero */}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    {/* Other */}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+      </div>
+    </div>
+    <Footer />
+  </div>
+);
+
+export default App;
---
+++ src/components/Stats.jsx
@@ -0,0 +1,25 @@
+import { stats } from "../constants";
+import styles from "../style";
+
+const Stats = () => (
+  <section
+    className={`${styles.flexCenter} sm:flex-row flex-col flex-wrap sm:mb-20 mb-6`}
+  >
+    {stats.map((stat) => (
+      <div
+        key={stat.id}
+        className={`flex-1 flex justify-start items-center sm:flex-row flex-col m-3`}
+      >
+        <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
+          {stat.value}
+        </h4>
+
+        <p className="font-poppins flex-1 font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase xs:ml-3 ml-0 ">
+          {stat.title}
+        </p>
+      </div>
+    ))}
+  </section>
+);
+
+export default Stats;
